10452899500b66ca80120d1619163cb032f7ced4
TMTanmay1 <tanmaymeshram883@gmail.com>
2024-03-12 18:42:46 +0530

    Integration_api 1

--- src/components/Pages/AddServicePopup.js
+++ src/components/Pages/AddServicePopup.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../Styles/AddServicePopup.css';
+import axios from 'axios';
 
 function AddServicePopup({ onClose }) {
+  const [service , setService] = useState('');
+  const [service_price , setServicePrice] = useState('');
+  const [service_duration , setServiceDuration] = useState('');
+
+  const handleAddService = (e) => {
+    e.preventDefault();
+    console.log(service , service_price , service_duration);
+    const token = localStorage.getItem('token');
+   
+    axios.post("https://zggwtvrk-8000.inc1.devtunnels.ms/api/swalook/add/services/",{
+      service: service,
+      service_price: service_price,
+      service_duration: service_duration
+    }).then((res) => {    
+      console.log(res.data);
+      console.log("service added");
+
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
   return (
     <div className="popup_overlay">
     <div className="popup_container">
@@ -12,21 +34,23 @@ function AddServicePopup({ onClose }) {
         <button className="close_button" onClick={onClose}>X</button>
       </div>
       <hr></hr>
+      <form onSubmit={handleAddService}>
       <div className="sn1">
           <label htmlFor="service_name">Service Name:</label>
-          <input type="text" id="service_name" name="service_name" placeholder='Service Name' />
+          <input type="text" id="service_name" name="service_name" placeholder='Service Name' onChange={(e)=>setService(e.target.value)}/>
       </div>
       <div className="sn2">
           <label htmlFor="duration">Duration:</label>
-          <input type="text" id="duration" name="duration" placeholder="Duration (min)" />
+          <input type="text" id="duration" name="duration" placeholder="Duration (min)" onChange={(e)=>setServiceDuration(e.target.value)}/>
       </div>
       <div className="sn3">
           <label htmlFor="price">Price:</label>
-          <input type="text" id="price" name="price" placeholder="Price" />
+          <input type="text" id="price" name="price" placeholder="Price" onChange={(e)=>setServicePrice(e.target.value)}/>
       </div>
       <div className="sn_button_container">
           <button className="sn_save_button">Save</button>
         </div>
+      </form>
     </div>
   </div>
   );
--- src/components/Pages/AdminLogin.js
+++ src/components/Pages/AdminLogin.js
@@ -3,21 +3,33 @@ import '../Styles/AdminLogin.css'
 import { useNavigate } from 'react-router-dom';
 import ForgetPassword from './ForgetPassword';
 import Logo1 from '../../assets/S_logo.png'
+import axios from 'axios';
 
 function AdminLogin() {
   const navigate = useNavigate();
-  const [admin, setAdmin] = useState({
-    phoneNumber: '',
-    password: ''
-  });
+  const [mobileno , setMobileno] = useState('');
+  const [password , setPassword] = useState('');
+  
+  const handleAdminLogin = (e) => {
+    console.log(mobileno , password);
+    e.preventDefault();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setAdmin({
-      ...admin,
-      [name]: value
-    });
-  };
+    axios.post("https://zggwtvrk-8000.inc1.devtunnels.ms/api/swalook/login/",{
+      mobileno: mobileno,
+      password: password
+    })
+    
+    .then((res) => {
+      if(res.data.text === 'login successfull !'){
+        navigate('/admin/dashboard');
+      }
+      const token = res.data.csrf_token; 
+      localStorage.setItem('token', token);
+      console.log(res.data);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
 
     const handleResetPasswordClick = () => {
       
@@ -33,14 +45,15 @@ function AdminLogin() {
             </div>
             <div className='admin_form'>
             <h1 className='admin_login_head'>Admin Login</h1>
-            <form>
+            <form onSubmit={handleAdminLogin}>
           <div className="AL_input-group">
             <label htmlFor="phone-number">Phone Number:</label>
             <input
               type="tel"
               id="phone-number"
+              name="phoneNumber"
               placeholder="Enter your phone number"
-              onChange={handleChange}
+              onChange={(e) => setMobileno(e.target.value)}
             />
           </div>
           <div className="AL_input-group">
@@ -48,8 +61,9 @@ function AdminLogin() {
             <input
               type="password"
               id="password"
+              name="password"
                 placeholder="Enter your password"
-                onChange={handleChange}
+                onChange={(e)=> setPassword(e.target.value)}
             />
           </div>
           <p class="forgot-password">Forgot your password? <a onClick={handleResetPasswordClick}>Reset it</a></p>
